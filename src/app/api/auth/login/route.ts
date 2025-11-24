73287c5336a6735b8f5d12f07e6ad9f86a43a957
1misadb <imisa2345678@gmail.com>
2025-11-24 11:30:21 +0700

    laba10

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -48,7 +48,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       sameSite: 'lax',
       secure: process.env.NODE_ENV === 'production',
       path: '/',
-      maxAge: 60 * 60, // 1 hour
+      maxAge: 2 * 2, 
     });
 
     return response;
